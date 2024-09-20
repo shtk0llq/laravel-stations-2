4cf626bc0050c650a99a787fe145bf7ff49d5399
shtk0llq <shtk0llq@gmail.com>
2024-09-20 12:42:02 +0900

    station 2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PracticeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,16 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/practice', function () {
-    return response('practice');
-});
-
-Route::get('/practice2', function () {
-    $test = 'practice2';
-    return response($test);
-});
-
-Route::get('/practice3', function () {
-    $test = 'test';
-    return response($test);
-});
+Route::get('/practice', [PracticeController::class, 'sample']);
+Route::get('/practice2', [PracticeController::class, 'sample2']);
+Route::get('/practice3', [PracticeController::class, 'sample3']);
